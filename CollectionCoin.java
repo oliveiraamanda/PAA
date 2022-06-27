b956c4f77a9d702070a8a8453f1cf37ffe850e94
Amanda Oliveira <63080176+oliveiraamanda@users.noreply.github.com>
2022-06-27 20:43:48 -0300

    Add CollectionCoin

---
+++ CollectionCoin.java
@@ -0,0 +1,10 @@
+public class CollectionCoin {
+
+    public static void main(String[] args) {
+        int[][] tabuleiro = {{1,1,0,11},
+                          {1,2,2,1},
+                          {0,0,111,0}};
+    }
+
+
+}
